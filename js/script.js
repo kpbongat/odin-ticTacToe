f347ef26651fb3c74175d3a68ee8b71a0b5c8822
kpbongat <kpbongat.dev@gmail.com>
2024-06-09 16:24:41 +0800

    Implement module pattern in non-reusable factories

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
-function Gameboard () {
-    const board = [];
-    
-    for (let row = 0; row < 3; row++){
-        board.push([]);
-        for (let col = 0; col < 3; col++) {
-            board[row].push(Cell());
+const GameController = (function () {
+    const Gameboard = (function () {
+        const board = [];
+        
+        for (let row = 0; row < 3; row++){
+            board.push([]);
+            for (let col = 0; col < 3; col++) {
+                board[row].push(Cell());
+            }
         }
-    }
+        
+        function getBoard () {
+            return board;
     
-    function getBoard () {
-        return board;
+        }
+        return {getBoard};
+    })();
 
-    }
-    return {getBoard};
-}
+
+
+})();
 
 function Player (name, number) {
     const token = (number === 1) ? 'X' : 'O';
